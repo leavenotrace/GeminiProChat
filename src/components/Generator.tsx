41039b4bf49e31bc9a021594a12a2f0b24589414
leavenotrace <leavenotrace@foxmail.com>
2024-02-10 08:39:30 +0800

    update display

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -213,7 +213,7 @@ export default () => {
 
   return (
     <div my-6>
-      {/* beautiful coming soon alert box, position: fixed, screen center, no transparent background, z-index 100*/}
+      {/* beautiful coming soon alert box, position: fixed, screen center, no transparent background, z-index 100 */}
       <Show when={showComingSoon()}>
         <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-100">
           <div class="bg-white rounded-md shadow-md p-6">
@@ -261,7 +261,7 @@ export default () => {
           <textarea
             ref={inputRef!}
             onKeyDown={handleKeydown}
-            placeholder="Enter something..."
+            placeholder="发送消息给gemini..."
             autocomplete="off"
             autofocus
             onInput={() => {
@@ -272,7 +272,7 @@ export default () => {
             class="gen-textarea"
           />
           <button onClick={handleButtonClick} gen-slate-btn>
-            Send
+            发送
           </button>
           <button title="Clear" onClick={clear} gen-slate-btn>
             <IconClear />
